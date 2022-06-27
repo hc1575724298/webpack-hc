d0c21420391524d2287e6cdfd5ecff46a4b6e155
huchun <1575724298@qq.com>
2022-06-27 20:21:40 +0800

    添加jquery,修改样式

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-06-27 19:38:37
  * @LastEditors: sj
- * @LastEditTime: 2022-06-27 19:39:49
+ * @LastEditTime: 2022-06-27 20:19:58
  */
 import './banner'
 import './search'
+import $ from 'jquery'
+$('#swiper').css('background', 'blue')
