b27be00f593936628e05de29f69382d79e55e142
huchun <1575724298@qq.com>
2022-06-27 21:29:00 +0800

    处理了图片和字体图标

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-06-27 19:38:37
  * @LastEditors: sj
- * @LastEditTime: 2022-06-27 21:01:36
+ * @LastEditTime: 2022-06-27 21:24:28
  */
 import './banner'
 import './search'
@@ -13,3 +13,19 @@ import $ from 'jquery'
 $('#swiper').css('background', 'blue')
 import './index.css'
 import './index.less'
+
+// index.js
+import imgUrl from './assets/1.gif';
+
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+import imgUrl1 from './assets/logo_small.png';
+
+let img1 = document.createElement('img');
+img1.src = imgUrl1;
+document.body.appendChild(img1);
+
+//字体图标
+import './assets/fonts/iconfont.css'
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-06-27 20:25:55
  * @LastEditors: sj
- * @LastEditTime: 2022-06-27 21:02:36
+ * @LastEditTime: 2022-06-27 21:27:12
  */
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
@@ -23,7 +23,7 @@ module.exports = {
       template: './public/index.html', // 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
       filename: 'index.html' // 生成文件的名称
     }),
-    new CleanWebpackPlugin()
+    // new CleanWebpackPlugin()
   ],
   devServer: {
     open: true,
@@ -37,6 +37,45 @@ module:{
 {
   test: /\.less$/, // 匹配执行类型的文件
   use: [ "style-loader", "css-loader", 'less-loader']
+    },
+    // webpack4
+    // {
+    //   test: /\.(png|jpg|gif|jpeg)$/i,
+    //  use:[
+    //   {
+    //     loader: 'url-loader',
+    //     options: {
+    //       limit: 8 * 1024,
+    //     },
+    //   }
+    //  ]
+    // }
+    // webpack5
+    {
+      test: /\.(png|jpg|gif|jpeg)$/i,
+      type: 'asset',
+    },
+    //   { // webpack5默认内部不认识这些文件, 所以当做静态资源直接输出即可
+    //     test: /\.(eot|svg|ttf|woff|woff2)$/,
+    //     type: 'asset/resource',
+    //     generator: {
+    //       filename: 'font-[name].[hash:6][ext]'
+    //     }
+    // }
+    { // 处理字体图标的解析
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 2 * 1024,
+            // 配置输出的文件名
+            name: '[name].[ext]',
+            // 配置输出的文件目录
+            outputPath: "fonts/"
+          }
+        }
+      ]
 }
   ]
 }
